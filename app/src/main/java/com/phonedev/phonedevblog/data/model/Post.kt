d0e9a83d2faf4ceb331880cf5f0c232b092fecd7
YamiLaxus <osbaldoez@hotmail.com>
2021-07-10 23:47:44 -0600

    Add post_description

--- app/src/main/java/com/phonedev/phonedevblog/data/model/Post.kt
+++ app/src/main/java/com/phonedev/phonedevblog/data/model/Post.kt
@@ -4,4 +4,5 @@ data class Post(
         val profile_picture: String = "",
         val profile_name: String = "",
         val post_timestamp: com.google.firebase.Timestamp? = null,
-        val post_image: String = "")
+        val post_image: String = "",
+        val post_description: String = "")
